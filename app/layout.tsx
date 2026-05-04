4b75c875d2661ab587009f9d2563f0961001a47b
nazmul910 <islamnasir910@gmail.com>
2026-05-04 20:08:16 +0600

    feat: integrate Redux Provider into RootLayout for state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import { store } from "../lib/store";
+import { Provider } from "react-redux";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="">
+        <Provider store={store}>{children}</Provider>
+      </body>
     </html>
   );
 }
