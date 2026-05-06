51223e6c68c743714e3dd0317a786b83a7de87a8
nazmul910 <islamnasir910@gmail.com>
2026-05-06 19:48:24 +0600

    refactor: restructure authSlice to improve state management and add error handling

--- features/auth/authSlice.ts
+++ features/auth/authSlice.ts
@@ -1,27 +1,69 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { registerUser, loginUser } from "./authApi";
+import { clear, log } from "console";
 
-const initialState = {
+interface AuthStat{
+  user:any | null;
+  token: string | null;
+  loading: boolean;
+  error: string | null;
+}
+
+
+const initialState:AuthStat = {
   user: null,
   token: null,
+  loading: false,
+  error: null,
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name:"auth",
   initialState,
-  reducers: {
-    setUser(state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-
-      localStorage.setItem("token", action.payload.token);
-    },
-    logout(state) {
+  reducers:{
+    logout(state){
       state.user = null;
       state.token = null;
+      state.error = null;
       localStorage.removeItem("token");
     },
+    clearError(state){
+      state.error = null;
+    }
   },
-});
+  extraReducers:(builder) =>{
+    builder.addCase(registerUser.pending,(state) =>{
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(registerUser.fulfilled,(state,action) =>{
+      state.loading = false;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      localStorage.setItem("token",action.payload.token);
+    });
+    builder.addCase(registerUser.rejected,(state,action) =>{
+      state.loading = false;
+      state.error = action.payload as string;
+    });
+    builder.addCase(loginUser.pending,(state) =>{
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(loginUser.fulfilled,(state,action) =>{
+      state.loading = false;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      localStorage.setItem("token",action.payload.token);
+    });
+    builder.addCase(loginUser.rejected,(state,action) =>{
+      state.loading = false;
+      state.error = action.payload as string;
+    });
+  }
+})
 
-export const { setUser, logout } = authSlice.actions;
+export const {logout,clearError} = authSlice.actions;
+
 export default authSlice.reducer;
+
